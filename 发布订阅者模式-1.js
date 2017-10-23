46510a19f02699e8bd66f4b51a01ef62d66506b3
myself <donghe587@139.com>
2017-10-23 22:22:25 +0800

    Create 发布订阅者模式-1.js

---
+++ 发布订阅者模式-1.js
@@ -0,0 +1,70 @@
+//  first
+
+var shoeObj = {}; // 定义发布者
+shoeObj.list = []; // 缓存列表 存放订阅者回调函数
+        
+// 增加订阅者
+shoeObj.listen = function(fn) {
+    shoeObj.list.push(fn);  // 订阅消息添加到缓存列表
+}
+
+// 发布消息
+shoeObj.trigger = function(){
+    for(var i = 0,fn; fn = this.list[i++];) {
+        fn.apply(this,arguments); 
+    }
+}
+// 小红订阅如下消息
+shoeObj.listen(function(color,size){
+    console.log("颜色是："+color);
+    console.log("尺码是："+size);  
+});
+
+// 小花订阅如下消息
+shoeObj.listen(function(color,size){
+    console.log("再次打印颜色是："+color);
+    console.log("再次打印尺码是："+size); 
+});
+shoeObj.trigger("红色",40);
+shoeObj.trigger("黑色",42);
+
+
+//two
+
+var shoeObj = {}; // 定义发布者
+shoeObj.list = []; // 缓存列表 存放订阅者回调函数
+        
+// 增加订阅者
+shoeObj.listen = function(key,fn) {
+    if(!this.list[key]) {
+        // 如果还没有订阅过此类消息，给该类消息创建一个缓存列表
+        this.list[key] = []; 
+    }
+    this.list[key].push(fn);  // 订阅消息添加到缓存列表
+}
+
+// 发布消息
+shoeObj.trigger = function(){
+    var key = Array.prototype.shift.call(arguments); // 取出消息类型名称
+    var fns = this.list[key];  // 取出该消息对应的回调函数的集合
+
+    // 如果没有订阅过该消息的话，则返回
+    if(!fns || fns.length === 0) {
+        return;
+    }
+    for(var i = 0,fn; fn = fns[i++]; ) {
+        fn.apply(this,arguments); // arguments 是发布消息时附送的参数
+    }
+};
+
+// 小红订阅如下消息
+shoeObj.listen('red',function(size){
+    console.log("尺码是："+size);  
+});
+
+// 小花订阅如下消息
+shoeObj.listen('block',function(size){
+    console.log("再次打印尺码是："+size); 
+});
+shoeObj.trigger("red",40);
+shoeObj.trigger("block",42);
